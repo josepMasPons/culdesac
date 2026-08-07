1608ea60ad2150f5a6351e45e78d25d10490db32
josep Mas <jmas2011@gmail.com>
2026-08-07 17:53:42 +0200

    c├▓pia autom├ática dv. 07/08/2026 17:53:42,08

---
+++ autopush.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d c:\Users\Neus-Josep\Desktop\react\culdesac
+
+git add .
+
+Git diff --cached –-quiet
+if errorlevel 1 (
+git commit -m "còpia automàtica %date% %time%"
+git push  )
+
+
