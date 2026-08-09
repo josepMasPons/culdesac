956bc0f2538984d22e3ca44f96a0fd85f9126517
josep Mas <jmas2011@gmail.com>
2026-08-09 18:17:18 +0200

    copia automatica dg. 09/08/2026 18:17:18,85

--- autopush.bat
+++ autopush.bat
@@ -1,14 +1,17 @@
 @echo off
-cd /d c:\Users\Neus-Josep\Desktop\react\culdesac
+cd /d C:\Users\Neus-Josep\Desktop\react\culdesac
 
 git add .
 
-Git diff --cached --quiet
+git diff --cached --quiet
 if errorlevel 1 (
-git commit -m "copia automatica %date% %time%"
-echo feta una copia el  %date% a les  %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
-git push  )
-else (
-echo Llançat autopush.bat, però no feta cap còpia  el  %date% a les  %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
-)
+    git commit -m "copia automatica %date% %time%"
+    echo feta una copia el %date% a les %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
+    git push
 
+    msg %username% /TIME:5 "Còpia automàtica feta correctament"
+)
+else (
+    echo Llançat autopush.bat, però no feta cap còpia el %date% a les %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
+    msg %username% /TIME:5 "Llançat autopush.bat, però no feta Còpia automàtica "
+)
