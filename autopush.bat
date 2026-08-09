36bc82e4c81398752e9fa042480d59aac4cdea40
josep Mas <jmas2011@gmail.com>
2026-08-09 20:21:11 +0200

    Copia automatica dg. 09/08/2026 20:21:11,89

--- autopush.bat
+++ autopush.bat
@@ -9,13 +9,15 @@ REM Comprovar si hi ha canvis preparats per fer commit
 git diff --cached --quiet
 
 if %errorlevel% equ 0 (
-    echo No hi ha canvis. No es fa cap copia.
+    echo "No hi han canvis "  %date% %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
+   REM  echo No hi ha canvis. No es fa cap copia.
 ) else (
     REM Hi ha canvis: fer commit
     git commit -m "Copia automatica %date% %time%"
 
     if errorlevel 1 (
-        echo ERROR: no s'ha pogut fer el commit.
+         echo "ERROR no feta la copia : "  %date% %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
+        REM echo ERROR: no s'ha pogut fer el commit.
         
         powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$wsh = New-Object -ComObject WScript.Shell; $wsh.Popup('ERROR en la copia a GitHub',5,'GitHub - Autopush',16) | Out-Null"
     ) else (
@@ -23,11 +25,13 @@ if %errorlevel% equ 0 (
         git push
 
         if errorlevel 1 (
-            echo ERROR: el git push ha fallat.
+            echo "ERROR el git ha fallat : "  %date% %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
+            REM echo ERROR: el git push ha fallat.
 
             powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$wsh = New-Object -ComObject WScript.Shell; $wsh.Popup('ERROR en la copia a GitHub',5,'GitHub - Autopush',16) | Out-Null"
         ) else (
-            echo COPIA CORRECTA: git push completat.
+             echo "COPIA CORRECTA amb data : "  %date% " a les " %time% >> C:\Users\Neus-Josep\Desktop\react\culdesac\autopush.log
+            REM echo COPIA CORRECTA: git push completat.
 
             powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$wsh = New-Object -ComObject WScript.Shell; $wsh.Popup('Copia a GitHub completada correctament',3,'GitHub - Autopush',64) | Out-Null"
         )
